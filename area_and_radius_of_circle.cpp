0f9cc0e77dd58deea187c20d528f9efa87136ee4
Shaikbismillabanu <skbismillabanu@gmail.com>
2025-11-27 23:23:14 +0530

    Add files via upload

---
+++ area_and_radius_of_circle.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int radius;
+	float pie=3.14;
+	printf("enter the radius");
+	scanf("%d",&radius);
+    int ("area of the circle=pie*radius*radius");
+	printf("the area of the circle is %f",area);
+	scanf("%d",&area);
+	return 0;
+}
